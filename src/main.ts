aca974d3300cc601c3a6f57e5f6615a08af9cb21
andreamazzini3 <a.mazzini3@campus.unimib.it>
2023-11-16 12:21:28 +0100

    add interceptLogger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { LoggingInterceptor } from './common/interceptors/logging.interceptors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.useGlobalInterceptors(new LoggingInterceptor());
   await app.listen(8080);
 }
 bootstrap();
